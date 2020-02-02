19b9c0d07d72fa4799ffe066a8b2e6cf63e1aee2
tmainelli <58540597+tmainelli@users.noreply.github.com>
2020-02-02 15:14:26 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Assignment 7</title>
+  <meta charset="utf-8">
+</head>
+<body>
+<!-- Tony Mainelli
+     assignment7.html
+     INFO2340.WW
+     Thoendel
+     1/28/20
+-->
+<?php
+    $dinner = 'jambalaya';
+    $rating = '2';
+    $place = 'Aksarben Village';
+    $animal = 'dog';
+?>
+
+<h1>Good morning, Nebraska!</h1>
+<div>Last night I ate <?php echo $dinner; ?>.</div>
+<div>While it was good, it wasn't perfect. I'd give it <?php echo $rating; ?> stars out of 4.</div>
+<div>Tomorrow I plan to go to the <?php echo $place; ?>.</div>
+<div>My favorite animal is the <?php echo $animal; ?>.</div>
+
+</body>
+</html>
